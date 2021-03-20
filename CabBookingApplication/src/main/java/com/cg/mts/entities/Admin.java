1966a86e02f4022c075d507700d09d3c1b680558
kavyareddy2000 <79070791+kavyareddy2000@users.noreply.github.com>
2021-03-20 11:20:32 +0530

    Added Admin Pojo class

--- CabBookingApplication/src/main/java/com/cg/mts/entities/Admin.java
+++ CabBookingApplication/src/main/java/com/cg/mts/entities/Admin.java
@@ -2,4 +2,22 @@ package com.cg.mts.entities;
 
 public class Admin extends AbstractUser {
 	private int adminId;
+
+	public int getAdminId() {
+		return adminId;
+	}
+
+	public void setAdminId(int adminId) {
+		this.adminId = adminId;
+	}
+
+	public Admin() {
+	}
+
+	public Admin(int adminId) {
+		super();
+		this.adminId = adminId;
+	}
+	
+	
 }
