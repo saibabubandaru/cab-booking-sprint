8e7159d1ad0948403117673e5d3b2b67b858630f
MaddalaPraharshini <66952762+MaddalaPraharshini@users.noreply.github.com>
2021-03-20 11:59:07 +0530

    added customer pojo class

--- CabBookingApplication/src/main/java/com/cg/mts/entities/Customer.java
+++ CabBookingApplication/src/main/java/com/cg/mts/entities/Customer.java
@@ -2,4 +2,22 @@ package com.cg.mts.entities;
 
 public class Customer extends AbstractUser {
 	private int customerId;
+
+	public int getCustomerId() {
+		return customerId;
+	}
+
+	public void setCustomerId(int customerId) {
+		this.customerId = customerId;
+	}
+	
+	public Customer() {
+		
+	}
+
+	public Customer(int customerId) {
+		super();
+		this.customerId = customerId;
+	}
+	
 }
