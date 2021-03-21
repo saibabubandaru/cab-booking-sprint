1abd8be7bd58bac04ea1b6fabbb0d8dee446b7be
sumavarshitha3099 <79912647+sumavarshitha3099@users.noreply.github.com>
2021-03-21 20:03:06 +0530

    Update Cab.java

--- CabBookingApplication/src/main/java/com/cg/mts/entities/Cab.java
+++ CabBookingApplication/src/main/java/com/cg/mts/entities/Cab.java
@@ -4,4 +4,22 @@ public class Cab {
 	private int cabId;
 	private String carType;
 	private float perKmRate;
+	public int getCabId() {
+		return cabId;
+	}
+	public void setCabId(int cabId) {
+		this.cabId = cabId;
+	}
+	public String getCarType() {
+		return carType;
+	}
+	public void setCarType(String carType) {
+		this.carType = carType;
+	}
+	public float getPerKmRate() {
+		return perKmRate;
+	}
+	public void setPerKmRate(float perKmRate) {
+		this.perKmRate = perKmRate;
+	}
 }
