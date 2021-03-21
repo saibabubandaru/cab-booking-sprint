fd9a14f8bf03d16ad370477cfe4b48088ef16b0d
shadow-prince <59025976+shadow-prince@users.noreply.github.com>
2021-03-21 19:56:12 +0530

    🎈 Added Getters&Setters to TripBooking.jav

--- CabBookingApplication/src/main/java/com/cg/mts/entities/TripBooking.java
+++ CabBookingApplication/src/main/java/com/cg/mts/entities/TripBooking.java
@@ -13,4 +13,64 @@ public class TripBooking {
 	private boolean status;
 	private float distanceInKm;
 	private float bill;
+	public int getTripBookingId() {
+		return tripBookingId;
+	}
+	public void setTripBookingId(int tripBookingId) {
+		this.tripBookingId = tripBookingId;
+	}
+	public int getCustomerId() {
+		return customerId;
+	}
+	public void setCustomerId(int customerId) {
+		this.customerId = customerId;
+	}
+	public Driver getDriver() {
+		return driver;
+	}
+	public void setDriver(Driver driver) {
+		this.driver = driver;
+	}
+	public String getFromLocation() {
+		return fromLocation;
+	}
+	public void setFromLocation(String fromLocation) {
+		this.fromLocation = fromLocation;
+	}
+	public String getToLocation() {
+		return toLocation;
+	}
+	public void setToLocation(String toLocation) {
+		this.toLocation = toLocation;
+	}
+	public LocalDateTime getFromDateTime() {
+		return fromDateTime;
+	}
+	public void setFromDateTime(LocalDateTime fromDateTime) {
+		this.fromDateTime = fromDateTime;
+	}
+	public LocalDateTime getToDateTime() {
+		return toDateTime;
+	}
+	public void setToDateTime(LocalDateTime toDateTime) {
+		this.toDateTime = toDateTime;
+	}
+	public boolean isStatus() {
+		return status;
+	}
+	public void setStatus(boolean status) {
+		this.status = status;
+	}
+	public float getDistanceInKm() {
+		return distanceInKm;
+	}
+	public void setDistanceInKm(float distanceInKm) {
+		this.distanceInKm = distanceInKm;
+	}
+	public float getBill() {
+		return bill;
+	}
+	public void setBill(float bill) {
+		this.bill = bill;
+	}
 }
