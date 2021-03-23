08cfc6e6984ab6aca9f1c67f0c9902c6192f9499
shadow-prince <59025976+shadow-prince@users.noreply.github.com>
2021-03-23 21:27:22 +0530

    🎈 Added SuperclassMapping to Abstract class
    Everything working good!!🎉

--- CabBookingApplication/src/main/java/com/cg/mts/entities/AbstractUser.java
+++ CabBookingApplication/src/main/java/com/cg/mts/entities/AbstractUser.java
@@ -1,8 +1,12 @@
 package com.cg.mts.entities;
 
 import javax.persistence.Entity;
+import javax.persistence.Inheritance;
+import javax.persistence.InheritanceType;
+import javax.persistence.MappedSuperclass;
 
 
+@MappedSuperclass
 public abstract class AbstractUser {
 	private String username;
 	private String password;
