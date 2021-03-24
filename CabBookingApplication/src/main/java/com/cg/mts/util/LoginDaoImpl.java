2a43f19c5b782ac5cdc3be558f89b002f3dfd64e
shadow-prince <59025976+shadow-prince@users.noreply.github.com>
2021-03-24 11:18:43 +0530

    🎈 Added Login Module

--- CabBookingApplication/src/main/java/com/cg/mts/util/LoginDaoImpl.java
+++ CabBookingApplication/src/main/java/com/cg/mts/util/LoginDaoImpl.java
@@ -10,51 +10,54 @@ import org.springframework.stereotype.Repository;
 import com.cg.mts.entities.Admin;
 import com.cg.mts.entities.Customer;
 import com.cg.mts.entities.Driver;
+
 @Transactional
 @Repository
 public class LoginDaoImpl implements LoginDao {
 
 	@PersistenceContext
 	EntityManager em;
+
 	@Override
 	public String validateCredintials(Object obj) {
-          if(obj instanceof Customer) {
-        	  Customer c= (Customer) obj;
-        	  TypedQuery<Customer> q=em.createQuery("select c from Customer c where c.username=:uname and c.password=:pass",Customer.class);
-        	  q.setParameter("uname", c.getUsername());
-        	  q.setParameter("pass", c.getPassword());
-        	  
-        	  Customer cust=q.getSingleResult();
-        	  if(cust!=null)
-        		  return "success";
-        	  else 
-        		  return "Invalid";
-          }
-          else if(obj instanceof Driver) {
-        	  Driver d= (Driver) obj;
-        	  TypedQuery<Driver> q=em.createQuery("select c from Driver c where c.username=:uname and c.password=:pass",Driver.class);
-        	  q.setParameter("uname", d.getUsername());
-        	  q.setParameter("pass", d.getPassword());
-        	  
-        	  Driver cust=q.getSingleResult();
-        	  if(cust!=null)
-        		  return "success";
-        	  else 
-        		  return "Invalid";
-          }
-          else if(obj instanceof Admin) {
-        	  Admin d= (Admin) obj;
-        	  TypedQuery<Admin> q=em.createQuery("select c from Driver c where c.username=:uname and c.password=:pass",Admin.class);
-        	  q.setParameter("uname", d.getUsername());
-        	  q.setParameter("pass", d.getPassword());
-        	  
-        	  Admin cust=q.getSingleResult();
-        	  if(cust!=null)
-        		  return "success";
-        	  else 
-        		  return "Invalid";
-          }
-          return null;
+		if (obj instanceof Customer) {
+			Customer c = (Customer) obj;
+			TypedQuery<Customer> q = em.createQuery(
+					"select c from Customer c where c.username=:uname and c.password=:pass", Customer.class);
+			q.setParameter("uname", c.getUsername());
+			q.setParameter("pass", c.getPassword());
+
+			Customer cust = q.getSingleResult();
+			if (cust != null)
+				return "success";
+			else
+				return "Invalid";
+		} else if (obj instanceof Driver) {
+			Driver d = (Driver) obj;
+			TypedQuery<Driver> q = em.createQuery("select c from Driver c where c.username=:uname and c.password=:pass",
+					Driver.class);
+			q.setParameter("uname", d.getUsername());
+			q.setParameter("pass", d.getPassword());
+
+			Driver cust = q.getSingleResult();
+			if (cust != null)
+				return "success";
+			else
+				return "Invalid";
+		} else if (obj instanceof Admin) {
+			Admin d = (Admin) obj;
+			TypedQuery<Admin> q = em.createQuery("select a from Admin a where a.username=:uname and a.password=:pass",
+					Admin.class);
+			q.setParameter("uname", d.getUsername());
+			q.setParameter("pass", d.getPassword());
+
+			Admin cust = q.getSingleResult();
+			if (cust != null)
+				return "success";
+			else
+				return "Invalid";
+		}
+		return null;
 	}
 
 }
