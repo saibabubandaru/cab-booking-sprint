222284ecea25de0a7e4034189acd8479b6921e35
shadow-prince <59025976+shadow-prince@users.noreply.github.com>
2021-03-26 00:34:32 +0530

    🎭 Added Admin Testcases
    Co-Authored-By: MaddalaPraharshini <66952762+MaddalaPraharshini@users.noreply.github.com>

--- CabBookingApplication/src/test/java/com/cg/mts/CabBookingApplicationTests.java
+++ CabBookingApplication/src/test/java/com/cg/mts/CabBookingApplicationTests.java
@@ -9,38 +9,16 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestInstance;
 import org.junit.jupiter.api.TestInstance.Lifecycle;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
 import com.cg.mts.entities.Admin;
- 	
+
 @SpringBootTest
 @TestInstance(Lifecycle.PER_CLASS)
-class CabBookingApplicationTests extends AbstractTest{
+class CabBookingApplicationTests extends AbstractTest {
 
-	@Override
 	
-	@BeforeAll
-	public void setUp() {
-		super.setUp();
-	}
-	
-	@Test
-	public void getAdminUsername() throws Exception {
-		String uri = "/admin/2";
-		MvcResult mvcResult = mvc.perform(MockMvcRequestBuilders.get(uri)).andReturn();
-		int status = mvcResult.getResponse().getStatus();
-		assertEquals(200, status);
-		String content = mvcResult.getResponse().getContentAsString();
-		Admin admin = super.mapFromJson(content, Admin.class);
-		assertEquals("admin1", admin.getUsername());
-	}
-	@Test
-	public void getStausCode() throws Exception {
-		String uri = "/admin";
-		MvcResult mvcResult = mvc.perform(MockMvcRequestBuilders.get(uri)).andReturn();
-		int status = mvcResult.getResponse().getStatus();
-		assertEquals(200, status);
-	}
 
 }
---
+++ CabBookingApplication/src/test/java/com/cg/mts/CabManagementTests.java
@@ -0,0 +1,5 @@
+package com.cg.mts;
+
+public class CabManagementTests {
+
+}
